a2de18f813a29afe649a1b3ae706e65278b9523d
KevinMarete55443 <kevomarete@gmail.com>
2014-05-14 11:49:46 +0300

    changes

--- application/modules/user/controllers/user.php
+++ application/modules/user/controllers/user.php
@@ -9,21 +9,45 @@ class User extends MY_Controller {
 	}
 
 	public function index() {
-		$data['hide_menu']='';
-		$data['hide_sidemenu']='';
+		$data['hide_menu'] = '';
+		$data['hide_sidemenu'] = '';
 		$data['content_view'] = 'user/login';
 		$data['title'] = 'webADT | Login';
 		$this -> load -> module('template');
 		$this -> template -> default_load($data);
 	}
-	
-	public function forgot_pass(){//load Forgot password view
-		$data['hide_menu']='';
-		$data['hide_sidemenu']='';
+
+	public function forgot_pass() {//load Forgot password view
+		$data['hide_menu'] = '';
+		$data['hide_sidemenu'] = '';
 		$data['content_view'] = 'user/forgot_password';
 		$data['title'] = 'webADT | Forgot Password';
 		$this -> load -> module('template');
 		$this -> template -> default_load($data);
+	}
+
+	public function test_ar() {
+		$this -> load -> model('test');
+		$result = $this -> test -> get_users();
+		echo '<pre>';
+		print_r($result);
+		echo '</pre>';
+	}
+
+	public function test_dql() {
+		$this -> load -> model('test');
+		$result = $this -> test -> get_users_dql();
+		echo '<pre>';
+		print_r($result);
+		echo '</pre>';
+	}
+
+	public function test_obj() {
+		$this -> load -> model('test');
+		$result = $this -> test -> get_users_obj();
+		echo '<pre>';
+		print_r($result);
+		echo '</pre>';
 	}
 
 }
